d3653372a5da829ad9be995072e231b8356ca43e
AlexLoayza129 <alex_ht129@hotmail.com>
2022-11-22 12:19:04 -0500

    Deploy demo

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                    test: /\.js$/,
+                    loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    { loader : "css-loader", options: { importLoaders : 1 }},
+                    "postcss-loader",
+                ],
+            },
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+           template: 'index.html' 
+        })
+    ]
+}
